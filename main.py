b59ea05540adb404823f537be692858c1614f96f
Piotr Krzemiński <p.krzeminski7@gmail.com>
2023-12-16 22:56:41 +0100

    Add buttons

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import matplotlib
 matplotlib.use('TkAgg')  # Use the Tkinter backend - better for threading
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+from matplotlib.widgets import Button
 import threading
 import time
 import utils
@@ -31,7 +32,7 @@ class Bar:
                 # Thread-safe way to update the plot
                 self.ax.figure.canvas.draw_idle()
                 self.bar[0].set_height(self.value)
-            time.sleep(0.01)  # Update every 0.01 seconds
+            time.sleep(0.1)  # Update every 0.01 seconds
 
     def stop(self):
         self.running = False
@@ -43,8 +44,8 @@ class VisualQueue:
         self.queue = []
         self.rects = []
         self.texts = []
-        self.ax.set_xlim(0, max_size)
-        self.ax.set_ylim(0, 1)
+        self.ax.set_xlim(0, 1)
+        self.ax.set_ylim(0, max_size)
         self.ax.axis('off')
 
     def add(self, value):
@@ -68,11 +69,20 @@ class VisualQueue:
 
         # Draw new rectangles and texts
         for i, value in enumerate(self.queue):
-            rect = patches.Rectangle((i, 0), 1, 1, edgecolor='black', facecolor='skyblue')
-            self.ax.add_patch(rect)
-            self.rects.append(rect)
-            text = self.ax.text(i + 0.5, 0.5, str(value), ha='center', va='center', fontsize=12)
-            self.texts.append(text)
+            if i < len(self.rects):
+                # Reuse existing rectangle and text
+                self.rects[i].set_height(1)
+                self.texts[i].set_text(str(value))
+            else:
+                # Create new rectangle and text
+                rect = patches.Rectangle((0, i), 1, 1, edgecolor='black', facecolor='skyblue')
+                self.ax.add_patch(rect)
+                self.rects.append(rect)
+                text = self.ax.text(0.5, i + 0.5, str(value), ha='center', va='center', fontsize=12)
+                self.texts.append(text)
+
+def on_click(event, q):
+    q.add(f"t {0}, size {0}")
 
 # Load the config
 config = utils.load_config()
@@ -80,7 +90,7 @@ config = utils.load_config()
 # Set up the plot
 threads_count = config['threading']['count']
 queue_count = config['queue']['count']
-fig, axs = plt.subplots(2, max(threads_count, queue_count), figsize=(10, 5))
+fig, axs = plt.subplots(2, max(threads_count, queue_count), figsize=(10, 8))
 # fig.tight_layout()
 fig.suptitle(config['visualizer']['title'], fontsize=config['visualizer']['title_font_size'])
 
@@ -91,11 +101,12 @@ if threads_count == 1 or queue_count == 1:
 # Set up the bars and queues
 bars = []
 queues = []
+buttons = []
 
 for i in range(max(threads_count, queue_count)):
     if i < threads_count:
         # Setup for bars
-        axs[0, i].set_ylim(0, 100)
+        axs[0, i].set_ylim(config['visualizer']['y_lim'])
         axs[0, i].set_yticks([])
         bar = Bar(i, axs[0, i])
         bars.append(bar)
@@ -105,8 +116,14 @@ for i in range(max(threads_count, queue_count)):
         queue = VisualQueue(axs[1, i])
         queues.append(queue)
 
+        # Setup for buttons
+        button_ax = plt.axes([0.15 + 0.2 * i, 0.04, 0.12, 0.05])
+        button = Button(button_ax, f"Add to queue {i}")
+        button.on_clicked(lambda event, q=queue: on_click(event, q))
+        buttons.append(button)
+
 if threads_count > 0:
-    axs[0, 0].set_yticks([0, 10, 50, 100])
+    axs[0, 0].set_yticks(config['visualizer']['y_ticks'])
     axs[0, 0].set_ylabel(config['visualizer']['y_label'])
 
     axs[1, 0].set_ylabel("Queues")
